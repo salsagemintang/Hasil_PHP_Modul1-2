9b1067c4ee43d0a02386c4c41b92c374315faa1e
salsagemintang <46420684+salsagemintang@users.noreply.github.com>
2019-02-20 14:18:33 +0700

    Add files via upload

---
+++ PHP_Variabel.php
@@ -0,0 +1,17 @@
+<?php
+    $nama = "Salsabila";
+    $sekolah = "SMK Telkom Malang";
+    $umur = 16;
+
+    //menampilkan nilai variabel
+    //contoh 1
+    echo "Halo namaku $nama usiaku $umur tahun";
+    echo "<br>";
+    echo "Aku sekolah di $sekolah";
+    echo "<br>";
+
+    //contoh 2
+    echo 'Halo namaku'.$nama. 'usiaku'.$umur. 'tahun';
+    echo "<br>";
+    echo 'Aku sekolah di' .$sekolah;
+?>
